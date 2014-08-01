5709c7ffa5c11cb1a91deae522bb35bd7fe021e0
rpedde <ron@pedde.com>
2014-08-01 10:53:51 -0500

    first pass at configure pass

---
+++ configure-staging-cluster.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+set -x
+
+CLUSTER_ID=${CLUSTER_ID:-JOB_ID}
+SWIFT_TYPE=${SWIFT_VENV_CLASS:-testing}
+SWIFT_VERSION=${SWIFT_BUNDLE_VERSION:-00049}
+ZVM_VERSION=${00056}
+
+[ -d /var/lib/jenkins/ansible-zwift ] || exit 1
+pushd /var/lib/jenkins/ansible-zwift
+
+[ -d venv ] || exit 1
+
+source venv/bin/activate
+
+[ -e env/jenkinsrc ] || exit 1
+
+set +x
+source env/jenkinsrc
+set -x
+
+if [ ! -e clusters/j${CLUSTER_ID}.json ]; then
+    echo "cluster j${CLUSTER_ID} does not exist"
+    exit 1
+fi
+
+cp job.template clusters/j${CLUSTER_ID}.json
+sed -i clusters/j${CLUSTER_ID}.json -e #@SWIFT_TYPE@#${SWIFT_TYPE}#
+sed -i clusters/j${CLUSTER_ID}.json -e #@SWIFT_VERSION@#$SWIFT_VERSION#
+sed -i clusters/j${CLUSTER_ID}.json -e #@ZVM_VERSION@#${ZVM_VERSION}#
+
+./run -c j${CLUSTER_ID} -e @clusters/j${CLUSTER_ID}.json ./configure.yml
+
+popd
