e00e3c3a0cb1e8a9824a097951baa9e9bd94c84e
rpedde <ron@pedde.com>
2014-08-04 14:14:14 -0500

    dont emit creds

--- test-staging-cluster.sh
+++ test-staging-cluster.sh
@@ -8,7 +8,9 @@ source common.sh
 pip install python-swiftclient
 
 ./run -c j${CLUSTER_ID} -e @clusters/j${CLUSTER_ID}.json -e rc_path=../env/j${CLUSTER_ID}-swift.env contrib/swiftrc.yml
+set +x
 source env/j${CLUSTER_ID}-swift.env
+set -x
 
 # upload test data
 pushd ${WORKSPACE}/scaffold
