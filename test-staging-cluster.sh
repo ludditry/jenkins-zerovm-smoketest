5f5878baf9595d0b88b484c4acb822650fde8104
rpedde <ron@pedde.com>
2014-08-04 15:09:58 -0500

    add test success marking

--- test-staging-cluster.sh
+++ test-staging-cluster.sh
@@ -5,6 +5,12 @@ set -x
 
 source common.sh
 
+SWIFT_TYPE=${SWIFT_TYPE:-testing}
+SWIFT_VERSION=${SWIFT_VERSION:-00049}
+ZVM_TYPE=${ZVM_TYPE:-zvm2-zpipes}
+ZVM_VERSION=${ZVM_VERSION:-00056}
+WORKSPACE=${WORKSPACE:-.}
+
 pip install python-swiftclient
 
 ./run -c j${CLUSTER_ID} -e @clusters/j${CLUSTER_ID}.json -e rc_path=../env/j${CLUSTER_ID}-swift.env contrib/swiftrc.yml
@@ -21,3 +27,7 @@ for d in *; do
 done
 
 popd
+
+# if we haven't died by now, we can mark these as tested.
+echo ${ZVM_VERSION} > /var/www/cbundles/${ZVM_TYPE}/tested-version
+echo ${SWIFT_VERSION} > /var/www/${SWIFT_TYPE}/tested-version
